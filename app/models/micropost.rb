f7359d4addafbe695213d17b63520b095fb82633
purp <threesutra@gmail.com>
2020-12-07 07:53:20 +0000

    for Heroku 2

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,7 +2,7 @@ class Micropost < ApplicationRecord
   belongs_to :user
   
   # Favorite用
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   
   validates :content, presence: true, length: { maximum: 255 }
 end
